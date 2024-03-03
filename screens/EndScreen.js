27ec577cf5bbb9fb63f2d29ca15e5f60ca1c2be7
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-03 13:23:37 -0700

    intial_setup: Added start game and end screen files

---
+++ screens/EndScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const EndScreen = () => {
+  return (
+    <View>
+      <Text>Game Screen</Text>
+    </View>
+  );
+};
+
+export default EndScreen;
---
+++ screens/StartScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const StartScreen = () => {
+  return (
+    <View>
+      <Text>Start Screen</Text>
+    </View>
+  );
+};
+
+export default StartScreen;
