b87916d35b90927a53678f52bed14c27d8ec4a16
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-03 13:34:27 -0700

    initial_setup: Added button component

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const PrimaryButton = ({ text }) => {
+  return (
+    <View>
+      <Text>{text}</Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
